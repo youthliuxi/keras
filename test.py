5697eba56c222377748cf3d18a43c48fbbdc9d12
iron_baby <lx_einstein@sina.com>
2019-03-22 10:47:09 +0800

    add about test keras

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 np.random.seed(123)
+from keras.layers import Convolution2D
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.utils import np_utils
@@ -10,8 +11,29 @@ from keras.datasets import mnist
 # print(X_train.shape)
 import pylab
 from matplotlib import pyplot as plt
-plt.figure()
-plt.imshow(X_train[0])
-plt.axis('off') # 不显示坐标轴  
-pylab.show()  
+
+# plt.figure()
+# plt.imshow(X_train[0])
+# plt.axis('off') # 不显示坐标轴  
+# pylab.show()
+
 # print(X_train[0])
+X_train = X_train.reshape(X_train.shape[0], 1, 28, 28)
+X_test = X_test.reshape(X_test.shape[0], 1, 28, 28)
+# print(X_train.shape)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+# print(y_train.shape)
+# print(y_train[:10])
+# Convert 1-dimensional class arrays to 10-dimensional class matrices
+Y_train = np_utils.to_categorical(y_train, 10)
+Y_test = np_utils.to_categorical(y_test, 10)
+# print(Y_train.shape)
+	
+model = Sequential()
+model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(28,28,1)))
+print(model.output_shape)
